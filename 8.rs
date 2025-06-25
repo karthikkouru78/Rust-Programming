bfd5ac9a35eecbfcda793d4f763d1960f87ac567
karthikkouru78 <22pa1a0582@vishnu.edu.in>
2025-06-25 10:35:42 +0530

    Create 8.rs

---
+++ 8.rs
@@ -0,0 +1,21 @@
+8. Write a program to create an array of 10 elements and implement the following
+a. Create a of 2nd and 3rd element
+b. Omit the start index of the slice
+c. Omit the End Index of the Slice
+d. Omit both Start and End Index of the Slice
+
+fn main() {
+    let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+    let slice_a = &arr[1..3];
+    println!("Slice a (2nd and 3rd elements): {:?}", slice_a);
+
+    let slice_b = &arr[..3];
+    println!("Slice b (start omitted): {:?}", slice_b);
+
+    let slice_c = &arr[5..];
+    println!("Slice c (end omitted): {:?}", slice_c);
+
+    let slice_d = &arr[..];
+    println!("Slice d (start and end omitted): {:?}", slice_d);
+}
